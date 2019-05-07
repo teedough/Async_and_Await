9be0cde7964de4f71176229be46c33e6c8f79ccb
Tshediso Rathebe <rathebe.t@gmail.com>
2019-05-07 21:53:33 +0200

    First Commit

---
+++ Async_and_Await/Async_and_Await/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Async_and_Await
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.labelMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(264, 53);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(150, 47);
+            this.buttonProcess.TabIndex = 0;
+            this.buttonProcess.Text = "Process File";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // labelMsg
+            // 
+            this.labelMsg.AutoSize = true;
+            this.labelMsg.Location = new System.Drawing.Point(261, 141);
+            this.labelMsg.Name = "labelMsg";
+            this.labelMsg.Size = new System.Drawing.Size(0, 13);
+            this.labelMsg.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 270);
+            this.Controls.Add(this.labelMsg);
+            this.Controls.Add(this.buttonProcess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.Label labelMsg;
+    }
+}
+
---
+++ Async_and_Await/Async_and_Await/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Async_and_Await
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int CountCharacters()
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader("C:\\Users\\Teedo\\Desktop\\data.txt")) //addd your path here
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+
+            return count;
+        }
+        private async void buttonProcess_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountCharacters); //unit of work
+            task.Start(); //offload the method to this task 
+
+
+            labelMsg.Text = "Processing, Please wait...";
+            int count = await task;
+            labelMsg.Text = count.ToString() + " characters in file";
+        }
+    }
+}
